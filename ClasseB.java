8b570571088b05f4dc5a5f4f862e46ce9b6a9918
rsartorelli <renan.sartorelli@hotmail.com>
2017-07-18 11:55:58 -0300

    Versão 1.3b

---
+++ ClasseB.java
@@ -0,0 +1,28 @@
+Public class B {
+	
+	
+	private int B1;
+	private float B2;
+	private String B4;
+	
+	public String getB4(){
+		return B4;
+	}
+	public void setB4(String b4){
+		B4 = b4;
+	}
+	
+	public Int getB1(){
+		return B1;
+	}	
+	public float getB2(){
+		return B2;
+	}
+	public void setB1(int b1){
+		B1 = b1;
+	}	
+	public void setB2(float B2){
+		B2 = b2;
+	}
+	
+}
