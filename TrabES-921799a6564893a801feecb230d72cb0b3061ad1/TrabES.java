1b181171a739af847937b469d385cf7bba4925b6
rsartorelli <renan.sartorelli@hotmail.com>
2017-07-18 11:26:44 -0300

    Nova função Soma

---
+++ TrabES-921799a6564893a801feecb230d72cb0b3061ad1/TrabES.java
@@ -0,0 +1,35 @@
+Public class A {
+	
+	
+	private int A1;
+	private float A2;
+	private String A3;
+	
+	
+	public float Soma(){
+		return A1+A2;
+	}
+	
+	
+	public Int getA1(){
+		return A1;
+	}	
+	public float getA2(){
+		return A2;
+	}
+	public void setA1(int a1){
+		A1 = a1;
+	}	
+	public void setA2(float a2){
+		A2 = a2;
+	}
+	
+	public String getA3(){
+		return A3;
+	}
+	public void setA3(String a3){
+		A3 = a3;
+	}
+	
+	
+}
